beb6ca20ee41d72b2a7bda959baf622e2b3064b8
imaddine1 <hh.imad2016@gmail.com>
2022-02-05 12:47:33 +0100

    check inputs

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: iharile <iharile@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 10:44:32 by iharile           #+#    #+#             */
-/*   Updated: 2022/02/05 10:44:51 by iharile          ###   ########.fr       */
+/*   Updated: 2022/02/05 12:46:42 by iharile          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,77 +24,72 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-
-int	check_double(int *nbr, int len)
+int	compare_string(char *s1, char *s2)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	while (i < len)
-	{
-		j = i + 1;
-		while (j < len)
-		{
-			if (nbr[i] == nbr[j])
-				return (0);
-			j++;
-		}
+	while (s1[i] && (s1[i] == s2[i]))
 		i++;
+	if ((ft_strlen(s1) == i) && (ft_strlen(s2) == i))
+		return (0);
+	return (1);
+}
+
+int	check_double(char **str)
+{
+	int	j;
+	int	k;
+
+	j = 1;
+	while (str[j])
+	{
+		k = j + 1;
+		while (str[k])
+		{
+			if (!compare_string(str[j], str[k]))
+				return (0);
+			k++;
+		}
+		j++;
 	}
 	return (1);
 }
 
-int	check_int(char *str)
+int	check_int(char **str)
 {
 	int	i;
+	int	j;
 
-	i = 0;
+	j = 1;
 	if (!str)
 		return (0);
-	if (str[i] == '-')
-		i++;
-	while (str[i] && (str[i]>= '0' && str[i] <= '9' ))
-		i++;
-	if (i == ft_strlen(str))
-		return (1);
-	return (0);
+	while (str[j])
+	{
+		i = 0;
+		if (str[j][i] == '-')
+			i++;
+		while (str[j][i])
+		{
+			if (!(str[j][i] >= '0' && str[j][i] <= '9'))
+				return (0);
+			i++;
+		}
+		j++;
+	}
+	return (1);
 }
 
 int	main(int ac, char **av)
 {
 	int	i;
-	int	*integer;
 	int	j;
 
 	i = 1;
 	j = 0;
-	if (ac == 1)
+	if (ac == 1 || !check_int(av) || !check_double(av))
 	{
 		write (1, "Error\n", 6);
-		exit(1);
-	}
-	while (i < ac)
-	{
-		if (!check_int(av[i]))
-		{
-			write (1, "print just numbers\n", 19);
-			exit(1);
-		}
-		i++;
-	}
-	integer = malloc (sizeof(int) * (ac - 1));
-	if (!integer)
-		return (0);
-	i = 1;
-	while (av[i])
-	{
-		integer[i - 1] = ft_atoi(av[i]);
-		i++;
-	}
-	if (!check_double(integer, ac - 1))
-	{
-		write (1, "forbiden insert the same number\n", 32);
 		exit(1);
 	}
 	return (0);
