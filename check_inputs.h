47b792a7b43f3c814d1e312bbf86086221fb1aef
imaddine1 <hh.imad2016@gmail.com>
2022-02-06 12:01:28 +0100

    use libft and rename some files

--- check_inputs.h
+++ check_inputs.h
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   facile.h                                           :+:      :+:    :+:   */
+/*   check_inputs.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iharile <iharile@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 10:40:39 by iharile           #+#    #+#             */
-/*   Updated: 2022/02/05 10:44:24 by iharile          ###   ########.fr       */
+/*   Updated: 2022/02/06 11:58:48 by iharile          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FACILE_H
+#ifndef CHECK_INPUTS_H
 
-# define FACILE_H
+# define CHECK_INPUTS_H
 
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
 
-int	ft_atoi(char *str);
+int	compare_string(char *s1, char *s2);
+int	check_double(char **str);
+int	max_int(char *str);
+int	check_int(char **str);
 
 #endif
--- push_swap.c
+++ push_swap.c
@@ -6,104 +6,12 @@
 /*   By: iharile <iharile@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 10:44:32 by iharile           #+#    #+#             */
-/*   Updated: 2022/02/05 16:56:45 by iharile          ###   ########.fr       */
+/*   Updated: 2022/02/06 11:53:12 by iharile          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "facile.h"
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i])
-		i++;
-	return (i);
-}
-
-int	compare_string(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] && (s1[i] == s2[i]))
-		i++;
-	if ((ft_strlen(s1) == i) && (ft_strlen(s2) == i))
-		return (0);
-	return (1);
-}
-
-int	check_double(char **str)
-{
-	int	j;
-	int	k;
-
-	j = 1;
-	while (str[j])
-	{
-		k = j + 1;
-		while (str[k])
-		{
-			if (!compare_string(str[j], str[k]))
-				return (0);
-			k++;
-		}
-		j++;
-	}
-	return (1);
-}
-
-int	max_int(char *str)
-{
-	int		sign;
-	long	res;
-	int		i;
-
-	i = 0;
-	sign = 1;
-	res = 0;
-	if (str[i] == '-')
-	{
-		sign *= (-1);
-		i++;
-	}
-	while (str[i])
-	{
-		if (str[i] >= '0' && str[i] <= '9')
-			res = res * 10 + str[i] - '0';
-		else
-			return (0);
-		i++;
-	}
-	if (res * sign < -2147483648 || res * sign > 2147483647)
-		return (0);
-	return (1);
-}
-
-int	check_int(char **str)
-{
-	int		i;
-	int		j;
-
-	j = 1;
-	while (str[j])
-	{
-		i = 0;
-		if (str[j][i] == '-')
-			i++;
-		while (str[j][i])
-		{
-			if (!(str[j][i] >= '0' && str[j][i] <= '9') || !max_int(str[j]))
-				return (0);
-			i++;
-		}
-		j++;
-	}
-	return (1);
-}
+#include "check_inputs.h"
+#include "libft/libft.h"
 
 int	main(int ac, char **av)
 {
@@ -119,5 +27,6 @@ int	main(int ac, char **av)
 		write (1, "Error\n", 6);
 		exit(1);
 	}
+	
 	return (0);
 }
